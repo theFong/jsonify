c93cc9d4c785cbe3a5df0af5a29484307ffce4d7
theFOng <alecsanf@usc.edu>
2016-12-06 10:34:45 -0800

    fixed empty line bug

--- jsonify.rb
+++ jsonify.rb
@@ -10,23 +10,21 @@ File.open("example/example.txt", "r") do |f|
   @current_node = nil
   count = 0
   f.each_line do |line|
-  	if line == ""
-  		next
-  	end
-    line_split_data = line.split(" ")
-    if line_split_data[0].include? "$"
-      @current_node = Node.new line_split_data[1]
-      json.addChild @current_node
-      count = 0
-    else
-      if ARGV[2] == "-i"
-        @current_node.addKeyValue count, line_split_data[1...line_split_data.size].join(" ").to_s
+  	if line != "" and line != nil and line != "\n"
+      line_split_data = line.split(" ")
+      if line_split_data[0].include? "$"
+        @current_node = Node.new line_split_data[1]
+        json.addChild @current_node
+        count = 0
       else
-        @current_node.addKeyValue line_split_data[0], line_split_data[1...line_split_data.size].join(" ").to_s
+        if ARGV[2] == "-i"
+          @current_node.addKeyValue count, line_split_data[1...line_split_data.size].join(" ").to_s
+        else
+          @current_node.addKeyValue line_split_data[0], line_split_data[1...line_split_data.size].join(" ").to_s
+        end
+        count += 1
       end
-      count += 1
     end
-  
   end
 end
 
